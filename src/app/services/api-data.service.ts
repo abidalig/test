677165ce3443e0edfdad3de84d4e602cf51afd58
Abid Masood <61305201+abidalig@users.noreply.github.com>
2023-03-06 12:08:39 +0530

    Update api-data.service.ts

--- src/app/services/api-data.service.ts
+++ src/app/services/api-data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
+import { Observable } from 'rxjs/internal/Observable';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,21 +11,24 @@ export class ApiDataService {
 
   url = "https://jsonplaceholder.typicode.com/posts";
   
+  //memberData():Observable<[]>{
+  //return this.httpdata.get<[]>(this.url);
   memberData()
   {
-    return this.httpdata.get(this.url);
+      return this.httpdata.get<[]>(this.url);
   }
   saveUsers(data:any)
   {
    return this.httpdata.post(this.url,data);
 
   }
- /* memberData()
+
+  userData()
   {
     return [
     {name:'abd', email:'ab@gamil.com'},
     {name:'kjh', email:'ab@gamil.com'},
     {name:'xza', email:'ab@gamil.com'}
     ]
-  } */
+  } 
 }
